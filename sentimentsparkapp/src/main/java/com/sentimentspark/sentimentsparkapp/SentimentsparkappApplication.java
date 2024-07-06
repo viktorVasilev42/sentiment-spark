6ae622fe52f3c3ae1191831cfc32b71cf32c7eac
viktorVasilev42 <vasilevviktor03@gmail.com>
2024-07-07 01:26:50 +0200

    form checkbox test

--- sentimentsparkapp/src/main/java/com/sentimentspark/sentimentsparkapp/SentimentsparkappApplication.java
+++ sentimentsparkapp/src/main/java/com/sentimentspark/sentimentsparkapp/SentimentsparkappApplication.java
@@ -19,7 +19,7 @@ public class SentimentsparkappApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		PyScriptRunner pythonScriptRunner = new PyScriptRunner(List.of("META","GOOG"));
-		pythonScriptRunner.runScript();
+		// PyScriptRunner pythonScriptRunner = new PyScriptRunner(List.of("META","GOOG"));
+		// pythonScriptRunner.runScript();
 	}
 }
--- sentimentsparkapp/src/main/java/com/sentimentspark/sentimentsparkapp/controller/HomeController.java
+++ sentimentsparkapp/src/main/java/com/sentimentspark/sentimentsparkapp/controller/HomeController.java
@@ -1,15 +1,34 @@
 package com.sentimentspark.sentimentsparkapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/hello")
 public class HomeController {
 
     @GetMapping()
-    public String abc() {
+    public String abc(
+            @RequestParam(defaultValue = "0") int scraper,
+            @RequestParam(defaultValue = "false") boolean generate,
+            Model model
+    ) {
+        model.addAttribute("scraper", scraper);
+        model.addAttribute("generate", generate);
         return "home";
+    }
+
+    @PostMapping()
+    public String test(
+            @RequestParam(required = false) boolean one,
+            @RequestParam(required = false) boolean two
+    ) {
+        System.out.println("Checkbox one: " + one);
+        System.out.println("Checkbox two: " + two);
+        return "redirect:/hello?generate=true";
     }
 }
